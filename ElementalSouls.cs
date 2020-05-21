d59e6da548966b65c86110682029c6fda6b41b16
Jacob Sansbury <sansburyjacob@gmail.com>
2020-05-21 13:54:12 -0700

    Initial commit

---
+++ ElementalSouls.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace ElementalSouls
+{
+	public class ElementalSouls : Mod
+	{
+	}
+}
---
+++ Items/Soul.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace ElementalSouls.Items
+{
+    class Soul : ModItem
+    {
+    }
+
+    class IceSoul : Soul
+    {
+    } 
+
+    class FireSoul : Soul
+    {
+         
+    }
+
+    class WindSoul : Soul
+    {
+
+    }
+
+    class EarthSoul : Soul
+    {
+
+    }
+}
