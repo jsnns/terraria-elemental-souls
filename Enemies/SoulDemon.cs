dcdd8ba46d8f910b4c736c249e429cfb3463bbf6
Jacob Sansbury <sansburyjacob@gmail.com>
2020-05-21 15:53:51 -0700

    Set defaults and stats based on spawning player

--- Enemies/SoulDemon.cs
+++ Enemies/SoulDemon.cs
@@ -6,12 +6,14 @@ using System.Text;
 using System.Threading.Tasks;
 using Terraria;
 using Terraria.ModLoader;
-using Terraria.ModLoader.IO;
+using Terraria.ID;
 
 namespace ElementalSouls.Enemies
 {
     class IceSoulDemon : ModNPC
     {
+
+        private int lifeOfPlayer;
 
         public override void SetStaticDefaults()
         {
@@ -21,9 +23,16 @@ namespace ElementalSouls.Enemies
         public override void SetDefaults()
         {
             npc.friendly = false;
+            npc.noGravity = true;
+            npc.noTileCollide = true;
+            npc.DeathSound = SoundID.NPCDeath2;
+            npc.HitSound = SoundID.NPCHit2;
             npc.lifeMax = 100;
             npc.damage = 70;
-            npc.aiStyle = 3; // the zombie aiStyle for now.
+            npc.aiStyle = 22; // the Floaty Gross aiStyle
+            npc.width = 18;
+            npc.height = 40;
+            npc.defense = 6;
         }
 
         public override float SpawnChance(NPCSpawnInfo spawnInfo)
@@ -37,13 +46,24 @@ namespace ElementalSouls.Enemies
 
             int playerHealth = spawnInfo.player.statLifeMax;
 
-            if (correctBiome && !alreadySpawned && !ElementalSoulsWorld.IceSoulDemonDowned)
+            if (correctBiome && !alreadySpawned)
             {
+                lifeOfPlayer = spawnInfo.player.statLifeMax;
                 return 0.1f * (playerHealth / 100);
             } else
             {
                 return 0.0f;
             }
+        }
+
+        public override int SpawnNPC(int tileX, int tileY)
+        {
+            // setup the stats based on the last observed player
+            npc.lifeMax = (int)(lifeOfPlayer * 0.2);
+            npc.defense = (int)(lifeOfPlayer / 75);
+            npc.damage = (int)(lifeOfPlayer / 7);
+
+            return base.SpawnNPC(tileX, tileY);
         }
 
         public override void NPCLoot()
